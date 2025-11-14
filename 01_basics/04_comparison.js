410d7473fb0f995cab1cd568834dc31499848550
Sachin-Hipparge <sachinhipparge064@gmail.com>
2025-11-14 07:15:35 +0000

    Datatype summary

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -8,14 +8,31 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0); 
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0); 
 
-// ===  
+// // ===  
 
-console.log("2" === 2); 
+// console.log("2" === 2); 
+
+
+// datatype summary
+
+// non primitive type or reference type
+
+const heroes = [ "shaktiman", " batman", "spiderman"]
+let myObj = {
+    name: "sachin",
+    age: 20,
+}
+
+const myFunction = function(){
+
+    console.log("hello world");
+}
+console.log(typeof myObj);
